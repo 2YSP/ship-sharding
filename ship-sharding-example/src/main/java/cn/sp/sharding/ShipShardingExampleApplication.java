ac3be8c5c7b32ec50a1de0b90db1f3d5ea8ef0b3
ship <shi.peng@xyb2b.com>
2023-06-02 15:48:15 +0800

    fist commit

---
+++ ship-sharding-example/src/main/java/cn/sp/sharding/ShipShardingExampleApplication.java
@@ -0,0 +1,13 @@
+package cn.sp.sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipShardingExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipShardingExampleApplication.class, args);
+    }
+
+}
---
+++ ship-sharding-spring-boot-starter/src/test/java/cn/sp/sharding/ShipShardingSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sp.sharding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipShardingSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
