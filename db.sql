351b29b35277d5b99bc5f807e720100f913c190b
ship <1123804727@qq.com>
2023-06-10 16:21:09 +0800

    db.sql

---
+++ db.sql
@@ -0,0 +1,65 @@
+CREATE TABLE `t_order_2022_1`
+(
+    `id`          bigint(20) unsigned NOT NULL COMMENT '主键',
+    `order_code`  varchar(32) DEFAULT NULL COMMENT '订单号',
+    `create_time` bigint(20) NOT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `t_order_2022_2`
+(
+    `id`          bigint(20) unsigned NOT NULL COMMENT '主键',
+    `order_code`  varchar(32) DEFAULT NULL COMMENT '订单号',
+    `create_time` bigint(20) NOT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_order_2022_3`
+(
+    `id`          bigint(20) unsigned NOT NULL COMMENT '主键',
+    `order_code`  varchar(32) DEFAULT NULL COMMENT '订单号',
+    `create_time` bigint(20) NOT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_order_2022_4`
+(
+    `id`          bigint(20) unsigned NOT NULL COMMENT '主键',
+    `order_code`  varchar(32) DEFAULT NULL COMMENT '订单号',
+    `create_time` bigint(20) NOT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `t_order_2023_1`
+(
+    `id`          bigint(20) unsigned NOT NULL COMMENT '主键',
+    `order_code`  varchar(32) DEFAULT NULL COMMENT '订单号',
+    `create_time` bigint(20) NOT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_order_2023_2`
+(
+    `id`          bigint(20) unsigned NOT NULL COMMENT '主键',
+    `order_code`  varchar(32) DEFAULT NULL COMMENT '订单号',
+    `create_time` bigint(20) NOT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_order_2023_3`
+(
+    `id`          bigint(20) unsigned NOT NULL COMMENT '主键',
+    `order_code`  varchar(32) DEFAULT NULL COMMENT '订单号',
+    `create_time` bigint(20) NOT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_order_2023_4`
+(
+    `id`          bigint(20) unsigned NOT NULL COMMENT '主键',
+    `order_code`  varchar(32) DEFAULT NULL COMMENT '订单号',
+    `create_time` bigint(20) NOT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
